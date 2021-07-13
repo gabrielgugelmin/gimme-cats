506c9b7932b120a0849b8c18214fa4167eefd9c2
Gabriel Favilla Gugelmin <gaab.fg@gmail.com>
2021-07-12 22:22:20 -0300

    feat: created CatService

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const api = require('./services/api')
+const CatService = require('./services/Cat')
 const fs = require('fs');
 
 const getAndSaveFile = async (filename) => {
-  const { data } = await getCat();
+  const { data } = await CatService.getCat();
 
   data.pipe(fs.createWriteStream(`download/${filename}-${Date.now()}.jpg`));
 }
--- services/api.js
+++ services/api.js
@@ -1,13 +1,8 @@
 const axios = require('axios');
 
 const api = axios.create({
-    baseURL: 'https://cataas.com/',
-    responseType: 'stream',
-})
-
-getCat = async () => {
-    const response = await api.get('cat');
-    return response;
-}
+  baseURL: 'https://cataas.com/',
+  responseType: 'stream',
+});
 
 module.exports = api;
